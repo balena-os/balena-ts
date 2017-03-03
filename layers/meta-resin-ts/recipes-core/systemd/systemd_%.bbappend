balena-os/balena-ts

commit 635a0c2975e17c3ef8b2b6faceb4f961d63f3290

Remove support for the ts7700 board
Signed-off-by: Florin Sarbu <florin@resin.io>

layers/meta-resin-ts/recipes-core/systemd/systemd_%.bbappend
@@ -1,7 +1,0 @@
-FILESEXTRAPATHS_prepend_ts7700 := "${THISDIR}/${PN}:"
-
-SRC_URI_append_ts7700 = " file://0001-Do-not-ignore-the-nbd-interface.patch"
-
-do_install_append_ts4900() {
-    rm ${D}/${sysconfdir}/systemd/system.conf.d/watchdog.conf
-}

layers/meta-resin-ts/conf/machine/ts7700.conf
@@ -1,14 +1,0 @@
-#@TYPE: Machine
-#@NAME: Technologic Systems TS-7700 Board
-#@SOC: Marvel PXA16x
-#@DESCRIPTION: Machine configuration for PXA16x
-
-include conf/machine/include/tune-arm926ejs.inc
-
-PREFERRED_PROVIDER_virtual/kernel ?= "linux-pxa16x"
-PREFERRED_VERSION_linux-pxa16x ?= "3.14"
-PREFERRED_PROVIDER_virtual/bootloader ?= "mbr-pxa-bootstrap"
-
-KERNEL_IMAGETYPE = "zImage"
-
-hostname_pn-base-files = "ts-pxa"

layers/meta-resin-ts/conf/samples/local.conf.sample
@@ -1,6 +1,5 @@
 # Supported machines
 #MACHINE ?= "ts4900"
-#MACHINE ?= "ts7700"
 
 # More info meta-resin/README.md
 #RESIN_CONNECTABLE ?= "1"

layers/meta-resin-ts/recipes-bsp/mbr-pxa16-bootstrap/mbr-pxa16-bootstrap.bb
@@ -1,20 +1,0 @@
-DESCRIPTION = "MBR bootstrap"
-LICENSE = "Proprietary"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
-SECTION = "Bootloader"
-PROVIDES = "virtual/bootloader"
-
-SRC_URI = "file://bootstrap-code.img"
-S = "${WORKDIR}"
-
-inherit deploy
-
-do_deploy () {
-    install -d ${DEPLOYDIR}
-    install ${S}/bootstrap-code.img ${DEPLOYDIR}
-}
-
-addtask deploy after do_install before do_build
-
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-COMPATIBLE_MACHINE = "ts7700"

layers/meta-resin-ts/recipes-core/images/resin-image.bbappend
@@ -6,24 +6,3 @@ include resin-image.inc
 
 # The bootloader gets flashed by resin-image-flasher
 RESIN_IMAGE_BOOTLOADER_ts4900 = ""
-
-#
-# ts7700
-#
-IMAGE_FSTYPES_append_ts7700 = " resinos-img"
-
-IMAGE_CMD_resinos-img_append_ts7700 () {
-    bbnote "TS7700 specific resinos-img configuration"
-
-    # Burn the second stage bootloader
-    dd if=${DEPLOY_DIR_IMAGE}/bootstrap-code.img of=${RESIN_RAW_IMG} conv=notrunc ; sync ; sync
-
-    # Prepare raw partition for kernel
-    dd if=/dev/zero of=${RESIN_RAW_IMG} seek=1 count=$(expr ${RESIN_BOOT_SIZE_ALIGNED} \/ ${RESIN_IMAGE_ALIGNMENT}) conv=notrunc bs=$(expr ${RESIN_IMAGE_ALIGNMENT} \* 1024) ; sync ; sync
-
-    # Burn kernel
-    dd if=${DEPLOY_DIR_IMAGE}/zImage of=${RESIN_RAW_IMG} seek=1 conv=notrunc bs=$(expr ${RESIN_IMAGE_ALIGNMENT} \* 1024) ; sync ; sync
-
-    # Set non-fs part-type for the boot partition (this is required by the bootloader)
-    sfdisk -c  ${RESIN_RAW_IMG} 1 da
-}

layers/meta-resin-ts/recipes-core/systemd/systemd/0001-Do-not-ignore-the-nbd-interface.patch
@@ -1,46 +1,0 @@
-Mon Sep 17 00:00:00 2001
-From: Theodor Gherzan <theodor@resin.io>
-Date: Mon, 18 Jan 2016 17:18:43 +0100
-Subject: systemd: Do not ignore the nbd interface
-
-[THIS IS MEANT AS A TEMPORARY FIX]
-The ts7700 makes heavy use of the nbd interface. Including the sd being exposed
-as a nbd block, so we would like udev to create its usual links for this type
-of devices.
-
-Signed-off-by: Theodor Gherzan <theodor@resin.io>
----
- rules/60-persistent-storage.rules | 2 +-
- rules/99-systemd.rules.in         | 3 ---
- 2 files changed, 1 insertion(+), 4 deletions(-)
-
-diff --git a/rules/60-persistent-storage.rules b/rules/60-persistent-storage.rules
-index 0b14bb4..2f2cebf 100644
---- a/rules/60-persistent-storage.rules
-+++ b/rules/60-persistent-storage.rules
-@@ -6,7 +6,7 @@
- ACTION=="remove", GOTO="persistent_storage_end"
-
- SUBSYSTEM!="block", GOTO="persistent_storage_end"
--KERNEL!="loop*|mmcblk*[0-9]|msblk*[0-9]|mspblk*[0-9]|nvme*|hd*|sd*|sr*|vd*|xvd*|bcache*|cciss*|dasd*|ubd*", GOTO="persistent_storage_end"
-+KERNEL!="nbd*|loop*|mmcblk*[0-9]|msblk*[0-9]|mspblk*[0-9]|nvme*|hd*|sd*|sr*|vd*|xvd*|bcache*|cciss*|dasd*|ubd*", GOTO="persistent_storage_end"
-
- # ignore partitions that span the entire disk
- TEST=="whole_disk", GOTO="persistent_storage_end"
-diff --git a/rules/99-systemd.rules.in b/rules/99-systemd.rules.in
-index b66d727..888af1e 100644
---- a/rules/99-systemd.rules.in
-+++ b/rules/99-systemd.rules.in
-@@ -25,9 +25,6 @@ SUBSYSTEM=="block", ENV{DEVTYPE}=="disk", KERNEL=="md*", ATTR{md/array_state}=="
- # Ignore loop devices that don't have any file attached
- SUBSYSTEM=="block", KERNEL=="loop[0-9]*", ENV{DEVTYPE}=="disk", TEST!="loop/backing_file", ENV{SYSTEMD_READY}="0"
-
--# Ignore nbd devices in the "add" event, with "change" the nbd is ready
--ACTION=="add", SUBSYSTEM=="block", KERNEL=="nbd*", ENV{SYSTEMD_READY}="0"
--
- # We need a hardware independent way to identify network devices. We
- # use the /sys/subsystem path for this. Current vanilla kernels don't
- # actually support that hierarchy right now, however upcoming kernels
---
-2.1.4
-

commit 92601b1f3c047daa20aac98670eb265d98bc896d

systemd_%.bbappend: Do not add the watchdog conf file to the rootfilesystem
Commit 635a0c2975e17c3ef8b2b6faceb4f961d63f3290
mistakenly removed this file.
Re-adding it now

Signed-off-by: Florin Sarbu <florin@resin.io>

layers/meta-resin-ts/recipes-core/systemd/systemd_%.bbappend
@@ -1,0 +1,3 @@
+do_install_append_ts4900() {
+    rm ${D}/${sysconfdir}/systemd/system.conf.d/watchdog.conf
+}